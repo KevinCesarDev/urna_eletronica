c7dbfee54924a6dbf94aa59f9698f0c87ab7b662
Kevin Céssar <102882654+KevinCesarDev@users.noreply.github.com>
2023-06-29 20:07:15 -0300

    .

---
+++ urna_eletronica/src/main/java/com/mycompany/urna_eletronica/Urna_eletronica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.urna_eletronica;
+
+/**
+ *
+ * @author kevin
+ */
+public class Urna_eletronica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
